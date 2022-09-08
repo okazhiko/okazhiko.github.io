cdab8fd10b83a8688061a95bba1e2d6b625a93ec
okazhiko <o.kazhiko@gmail.com>
2022-09-08 22:26:46 +0900

    add index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const button1 = document.getElementById("button1");
+button1.style.display = "none";
 if (window.location.href == 'https://okazhiko.github.io/?display=block') {
 	button1.style.display = "block";
-} else {
-	button1.style.display = "none";
 }
