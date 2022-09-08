80c9271e8227e50b544673c9cf75d54e9108002c
okazhiko <o.kazhiko@gmail.com>
2022-09-08 22:23:31 +0900

    add index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-// 現在のURLからURLオブジェクトを生成（例として、https://example.com上で以下Javascriptを実行しているとする）
-const url = new URL(window.location.href);
-console.log(url.href); // https://example.com
-
-// キーを指定し、クエリパラメータを付与
-url.searchParams.set('display', 'none');
-console.log(url.href); // https://example.com?addParam=test
-
 const button1 = document.getElementById("button1");
-if (window.location.href == 'https://okazhiko.github.io?display=none') {
+if (window.location.href == 'https://okazhiko.github.io?display=block') {
+	button1.style.display = "block";
+} else {
 	button1.style.display = "none";
 }
