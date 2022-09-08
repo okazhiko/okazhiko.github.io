b9332e0ba513ef82a7b6fd8ac25d4f1a67703d91
okazhiko <o.kazhiko@gmail.com>
2022-09-08 19:10:11 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// 現在のURLからURLオブジェクトを生成（例として、https://example.com上で以下Javascriptを実行しているとする）
+const url = new URL(window.location.href);
+console.log(url.href); // https://example.com
+
+// キーを指定し、クエリパラメータを付与
+url.searchParams.set('display', 'none');
+console.log(url.href); // https://example.com?addParam=test
+
+const button1 = document.getElementById("button1");
+if (window.location.href == 'file:///Users/oyamakazuhiko/Documents/projects/myPage/index.html?display=none') {
+	button1.style.display = "none";
+}
